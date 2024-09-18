de0c0ebd7f9a04e1d2a57d5121259b62a82f4155
Saajvatz <ajvatzalejandro@gmail.com>
2024-09-18 08:15:06 -0600

    Rama actualizada

--- backEnde/view/DoctorView.java
+++ backEnde/view/DoctorView.java
@@ -9,18 +9,18 @@ import java.awt.*;
         private int[] pantalla = {1300, 800};
 
         public DoctorLayout() {
-            // Configuración de la ventana
+
             setSize(pantalla[0], pantalla[1]);
             setLayout(new BorderLayout());
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            // Crear el panel de encabezado
+
             JPanel headerPanel = new JPanel();
             headerPanel.setPreferredSize(new Dimension(pantalla[0], 60));
             headerPanel.setBackground(Color.DARK_GRAY);
             headerPanel.setLayout(new BorderLayout());
 
-            // JLabel para el nombre del hospital
+
             JLabel hospitalLabel = new JLabel("Nombre del Hospital");
             hospitalLabel.setForeground(Color.WHITE);
             hospitalLabel.setHorizontalAlignment(SwingConstants.LEFT);
@@ -34,7 +34,7 @@ import java.awt.*;
 
             // Panel para el logo
             JPanel logoPanel = new JPanel();
-            logoPanel.setPreferredSize(new Dimension(50, 50)); // Placeholder size for logo
+            logoPanel.setPreferredSize(new Dimension(40, 40)); // Placeholder size for logo
             logoPanel.setBackground(Color.LIGHT_GRAY);
             gbc.gridx = 0;
             gbc.gridy = 0;
@@ -48,7 +48,7 @@ import java.awt.*;
             gbc.gridheight = 1;
             userPanel.add(doctorNameLabel, gbc);
 
-      
+
             JLabel doctorSpecialtyLabel = new JLabel("Cardiología");
             gbc.gridx = 1;
             gbc.gridy = 1;
