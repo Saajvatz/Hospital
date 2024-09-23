e1b72008126a0a3152a0d0daaea9122a6c4c3530
Saajvatz <ajvatzalejandro@gmail.com>
2024-09-23 08:16:50 -0600

    Primera version menu Lateral

--- backEnde/view/DoctorView.java
+++ backEnde/view/DoctorView.java
@@ -59,6 +59,7 @@ import java.awt.*;
 
 
             add(headerPanel, BorderLayout.NORTH);
+            add(componentesMenuLateral(),BorderLayout.WEST);
 
 
             setVisible(true);
@@ -66,6 +67,41 @@ import java.awt.*;
 
         public static void main(String[] args) {
             new DoctorLayout();
+        }
+
+        private JPanel componentesMenuLateral() {
+            JPanel menuPanel = new JPanel();
+            menuPanel.setPreferredSize(new Dimension(250,pantalla[1]));
+            menuPanel.setBackground(Color.darkGray);
+
+            JPanel menu = new JPanel();
+            menu.setLayout(new GridLayout(5,1));
+
+
+
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.gridx = 0;
+
+           menu.add(op("Opcion 1"),gbc);
+           menu.add(op("Opcion 2"),gbc);
+           menu.add(op("Opcion 3"),gbc);
+            menu.add(op("Opcion 4"),gbc);
+            menu.add(op("Opcion 5"),gbc);
+            menuPanel.add(menu);
+            return menuPanel;
+
+
+        }
+
+        private JButton op (String texto) {
+            JButton op = new JButton(texto);
+
+
+           op.addActionListener(e -> {
+               System.out.println(texto);
+           });
+            return op;
         }
     }
 
